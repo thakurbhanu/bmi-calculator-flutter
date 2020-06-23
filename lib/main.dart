a6b468396a7419378609563e098ae522b4e01fbb
Thakur Bhanu Pradeep <58737223+thakurbhanu@users.noreply.github.com>
2020-06-23 12:14:52 +0530

    dfg

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: InputPage(),
     );
   }
@@ -21,13 +22,12 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Center(
+            child: Text('BMI CALCULATOR')
+        ),
       ),
       body: Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
